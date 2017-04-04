d61dc78f178d0b3462b0a53d7692c471e5495663
Daniel Cunningham <danielcunningham713@gmail.com>
2017-04-03 21:27:23 -0400

    Trying to output data

---
+++ src/python/data_creator.py
@@ -0,0 +1,32 @@
+# had to add these lines to find src directory - evan
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from TwitterSearch import *
+from src.python.twitter_caller import TwitterCaller
+from src.python.watson_nlu import WatsonNLU
+
+twitter = TwitterCaller("../twitter_creds")
+nlu = WatsonNLU("../watson_creds")
+tweet_count = 0
+max_tweet_count = 20
+
+file = open('data_set', 'w')
+
+for tweet in twitter.search(['Donald', 'Trump']):
+    if tweet_count > max_tweet_count:
+        break
+    else:
+        tweet_count += 1
+    try:
+        text = tweet['text']
+        tag_types = ["sentiment", "categories", "concepts", "emotion", "entities"]
+        annotations = nlu.annotate(text, tag_types)
+
+        file.write(annotations["sentiment"])
+        file.write(' ')
+        file.write(text)
+        file.write('\n')
+    except Exception as e:
+        print(e)
+        continue
