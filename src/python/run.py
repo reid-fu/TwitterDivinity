9584358be07c71915e2e8b125552b87babd47898
Evan Clark <clark.2095@osu.edu>
2017-04-03 23:46:17 -0400

    tweaked classify_nb()

--- src/python/run.py
+++ src/python/run.py
@@ -5,22 +5,10 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 
 # Naive Bayes implementation
-def classify_nb(data):
-    # partition data into test and train data sets
-    # EXPERIMENT with different splits
-    split_idx = int(len(data.tweets) * 0.2) # 20%
-    train_tweets = data.tweets[:split_idx]
-    train_labels = data.labels[:split_idx]
-    test_tweets = data.tweets[split_idx:]
-    test_labels = data.labels[split_idx:]
-    
-     # get training data
-    categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
-    train = fetch_20newsgroups(subset='train', categories = categories, shuffle=True, random_state=42)
-    
+def classify_nb(train_data, train_labels, test_data, test_labels):
     # come up with word counts
     count_vect = CountVectorizer()
-    x_train_counts = count_vect.fit_transform(train_tweets)
+    x_train_counts = count_vect.fit_transform(train_data)
     
     # come up with frequencies using tf
     # EXPIREMENT test tf vs. tfidf
@@ -33,16 +21,21 @@ def classify_nb(data):
     clf = MultinomialNB().fit(x_train_tf, train_labels)
     
     # get test data
-    x_new_counts = count_vect.transform(test_tweets)
+    x_new_counts = count_vect.transform(test_data)
     x_new_tf = tf_transformer.transform(x_new_counts)
     
     # classify
     predicted = clf.predict(x_new_tf)
-    for doc, category in zip(test_tweets, predicted):
+    for doc, category in zip(test_data, predicted):
         print("%r => %s" % (doc, train.target_names[category]))
 
 
+def classify_nn(train_data, train_labels, test_data, test_labels):
+    print("neural net here")    
+    
+
 if __name__ == '__main__':
+    # get data and split into train and test sets
     data_file = open("data_set")
     json_data = json.load(data_file)
     split_idx = int(len(json_data.tweets) * 0.8)
@@ -50,8 +43,20 @@ if __name__ == '__main__':
     train_labels = json_data.sentiment[:split_idx]
     test_data = json_data.tweets[split_idx+1:]
     test_labels = json_data.sentiment[split_idx:]
-    count_vect = CountVectorizer()
-    count_map = count_vect.fit_transform(json_data.data)
-    
+
     # test classifier with naive bayes
-    classify_nb(json_data.data)
+    classify_nb(train_data, train_labels, test_data, test_labels)
+    classify_nn(train_data, train_labels, test_data, test_labels)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
